3de96a6bda542e2b3dfdc7d78531270f960fded8
Suriya <true.many.032@gmail.com>
2025-02-28 12:32:36 +0700

    Assignment:JavaScript Loop

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,13 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
 let minScore;
-// Start coding here
 
+// Start coding here
+let index = 0;
+for (let i = 0; scores[i]; i++)
+{
+    if (scores[i] < scores[index])
+        index = i;
+}
+minScore = scores[index];
 console.log(minScore);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 let companyName = "TechUp";
 
 // Start coding here
+for (let i = 0; companyName[i]; i++)
+{
+    console.log(`Number ${i+1} character is ${companyName[i]}`);
+}
